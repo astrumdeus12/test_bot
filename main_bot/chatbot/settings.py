9bcdbfb706132c846487a29568e2474cf56925e6
astrumdeus12 <ghghuggvhb@gmail.com>
2024-12-23 00:24:41 +1000

    del comments

--- main_bot/chatbot/settings.py
+++ main_bot/chatbot/settings.py
@@ -1,11 +1,4 @@
-# import asyncio
 from config import sdk
 
 model = sdk.models.completions('yandexgpt')
 model = model.configure(temperature=0.5)
-# result = model.run("расскажи что такое квадратный корень")
-# async def message():
-#     result = await model.run("расскажи что такое квадратный корень")
-#     print( result.text)
-
-# asyncio.run(message())
